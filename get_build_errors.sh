746f7fedc30103530abbb8d0bbf6e65004066fa2
jonathon <jonathonwhite@protonmail.com>
2020-01-25 04:42:24 -0500

    get build errors works properly

--- get_build_errors.sh
+++ get_build_errors.sh
@@ -5,13 +5,12 @@
 # read the file from bottom to top
 tac Saved/Logs/UA2.log | while read -r line
 do
-	echo $line
+	echo $line | sed -e 's/\\/\//g' | sed -e 's/C:\//\/mnt\/c\//gI' | sed -e 's/users/Users/g' | sed -e 's/ua23/UA23/g' | sed -e 's/ua2/UA2/g' | sed -e 's/source/Source/g'
+
+	# echo $line
 
 	# if line contains an error
-	if [[ $line == *""* ]];
-
-
-
+	# if [[ $line == *""* ]];
 	# if line is the beginning
 	if [[ $line == *"Starting HotReload"* ]];
 	then
