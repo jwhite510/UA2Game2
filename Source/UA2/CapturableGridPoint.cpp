4b8d28760fc1513e9a47b68ad47b548c3e76face
jonathon <jonathonwhite@protonmail.com>
2020-02-02 13:51:27 -0500

    tiles show which vehicles are on it

---
+++ Source/UA2/CapturableGridPoint.cpp
@@ -0,0 +1,78 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CapturableGridPoint.h"
+#include "VehicleBase.h"
+
+// Sets default values
+ACapturableGridPoint::ACapturableGridPoint()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+        // CaptureVolume->OnComponentBeginOverlap.AddDynamic(this, &ACapturableGridPoint::OnOverlapBegin);
+
+}
+
+// Called when the game starts or when spawned
+void ACapturableGridPoint::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACapturableGridPoint::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+        UE_LOG(LogTemp, Warning, TEXT("vehicles on point %s:"), *GetName());
+        for(auto& _Vehicle : VehiclesOnGridPoint )
+        {
+          if(_Vehicle!=nullptr)
+          {
+            UE_LOG(LogTemp, Warning, TEXT("VehiclesOnGridPoint:%s"), *_Vehicle->GetName());
+          }
+        }
+
+}
+
+void ACapturableGridPoint::OverLapBegin(AActor* OverlappingActor)
+{
+  AVehicleBase* OverlappingVehicle = FindOverlappingVehicle(OverlappingActor);
+  if(OverlappingVehicle!=nullptr)
+  {
+    // check if vehicle is in list, if not add it
+    bool FoundVehicle = 0;
+    for(auto& _Vehicle : VehiclesOnGridPoint ){
+      if(_Vehicle == OverlappingVehicle)
+      {
+        FoundVehicle = 1;
+        break;
+      }
+    }
+    if(!FoundVehicle)
+    {
+      VehiclesOnGridPoint.Add(OverlappingVehicle);
+    }
+  }
+}
+AVehicleBase* ACapturableGridPoint::FindOverlappingVehicle(AActor* OverlappingActorIn)
+{
+  // AVehicleBase* OverlappingActorParent = Cast<AVehicleBase>(OverlappingActorIn->GetParentActor());
+  AVehicleBase* OverlappingActor = Cast<AVehicleBase>(OverlappingActorIn);
+
+  // if(OverlappingActorParent!=nullptr)
+  // {
+    // return OverlappingActorParent;
+
+  // }
+  if (OverlappingActor!=nullptr)
+  {
+    return OverlappingActor;
+
+  }
+  return nullptr;
+}
+void ACapturableGridPoint::OverLapEnd(AActor* OverlappingActor)
+{
+    VehiclesOnGridPoint.Remove(Cast<AVehicleBase>(OverlappingActor));
+}
---
+++ Source/UA2/CapturableGridPoint.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CapturableGridPoint.generated.h"
+
+class AVehicleBase;
+
+UCLASS()
+class UA2_API ACapturableGridPoint : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACapturableGridPoint();
+
+        UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Capture")
+        UStaticMeshComponent* CaptureVolume = nullptr;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+        UFUNCTION(BluePrintCallable, Category="Overlap")
+        void OverLapBegin(AActor* OverlappingActor);
+
+        UFUNCTION(BluePrintCallable, Category="Overlap")
+        void OverLapEnd(AActor* OverlappingActor);
+
+        AVehicleBase* FindOverlappingVehicle(AActor* OverlappingActor);
+
+        TArray<AVehicleBase*> VehiclesOnGridPoint;
+
+};
