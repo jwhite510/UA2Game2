6a0fd77f47a786a3cc1c3ac72efb085b148ac0ea
jonathon <jonathonwhite@protonmail.com>
2020-02-09 07:38:37 -0500

    move the host station

--- Source/UA2/HostStationNavMovementComponent.cpp
+++ Source/UA2/HostStationNavMovementComponent.cpp
@@ -4,25 +4,41 @@
 #include "HostStationNavMovementComponent.h"
 #include "GameFramework/Actor.h"
 #include "DrawDebugHelpers.h"
+#include "HostStation.h"
+#include "HostStationNavigationPawn.h"
 
 
 void UHostStationNavMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed)
 {
-  UE_LOG(LogTemp, Warning, TEXT("UHostStationNavMovementComponent RequestDirectMove called"));
-  UE_LOG(LogTemp, Warning, TEXT("im being called!!!!"));
-  DrawDebugLine(
-      GetWorld(),
-      Parent->GetActorLocation(),
-      Parent->GetActorLocation()+(100*MoveVelocity.GetSafeNormal()),
-      FColor(255,0,0), // color
-      true, //persitent
-      1.,// lifetime
-      1,// depth priority
-      60 // thickness
-      );
+  if(Parent!=nullptr)
+  {
+    // get parent host station
+    AHostStation* ThisHostStation = Cast<AHostStationNavigationPawn>(Parent)->ParentHostStation;
+    if(ThisHostStation!=nullptr)
+    {
+
+      FVector MoveDirection = ThisHostStation->GetActorLocation();
+      FVector _MoveVelocity = MoveVelocity;
+      _MoveVelocity.Z = 0;
+      MoveDirection+=(5.5*_MoveVelocity.GetSafeNormal());
+      ThisHostStation->SetActorLocation(MoveDirection);
+
+
+      DrawDebugLine(
+          GetWorld(),
+          Parent->GetActorLocation(),
+          Parent->GetActorLocation()+(100*MoveVelocity.GetSafeNormal()),
+          FColor(255,0,0), // color
+          true, //persitent
+          1.,// lifetime
+          1,// depth priority
+          60 // thickness
+          );
+    }
+  }
 
 }
-void UHostStationNavMovementComponent::FindParent(AActor *ParentIn)
+void UHostStationNavMovementComponent::FindParent(AActor* ParentIn)
 {
   Parent = ParentIn;
 }
